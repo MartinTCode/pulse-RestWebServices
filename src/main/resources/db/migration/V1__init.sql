5ffea38ea615b45fbe331147e656c095c6429e83
hilda033 <hilinn-4@student.ltu.se>
2025-11-16 21:19:18 +0100

    Small fixes to SQL statements

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -6,20 +6,20 @@ CREATE SCHEMA IF NOT EXISTS ladok;
 
 -- Epok Schema
 CREATE TABLE IF NOT EXISTS epok.course (
-    course_id INT PRIMARY KEY,
+    course_id VARCHAR(50) PRIMARY KEY,
     course_name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS epok.module (
     module_id INT PRIMARY KEY,
-    course_id INT REFERENCES epok.course(course_id),
+    course_id VARCHAR(50) REFERENCES epok.course(course_id),
     module_code VARCHAR(50) NOT NULL,
     module_name VARCHAR(100) NOT NULL
 );
 
 -- StudentITS Schema
 CREATE TABLE IF NOT EXISTS studentits.student_account (
-    student_id INT PRIMARY KEY,
+    student_id VARCHAR(50) PRIMARY KEY,
     personal_no VARCHAR(20) NOT NULL
 );
 
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS studentits.student_account (
 CREATE TABLE IF NOT EXISTS ladok.result (
     result_id INT PRIMARY KEY,
     personal_no VARCHAR(20) NOT NULL,
-    course_id INT NOT NULL,
+    course_id VARCHAR(50) NOT NULL,
     module_code VARCHAR(50) NOT NULL,
     exam_date DATE NOT NULL,
     grade VARCHAR(5) NOT NULL
